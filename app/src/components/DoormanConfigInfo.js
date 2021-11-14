82145d50972df07eb079d8d6974bf8d531781d7a
silostack <silostack@protonmail.com>
2021-11-14 17:49:25 -0500

    added display of # mint tokens left in doorman

--- app/src/components/DoormanConfigInfo.js
+++ app/src/components/DoormanConfigInfo.js
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { Program, Provider, web3, BN } from "@project-serum/anchor";
-import { preflightCommitment, programId, idl, connection, DOORMAN_CONFIG, DOORMAN_WHITELIST } from "../utils/config";
+import {
+   preflightCommitment,
+   programId,
+   idl,
+   connection,
+   DOORMAN_CONFIG,
+   DOORMAN_WHITELIST,
+   MINT
+} from "../utils/config";
 import {Button, Card, CardActionArea, CardActions, CardContent, Grid, Paper} from "@mui/material";
 import { styled } from '@mui/material/styles';
+const {
+   ASSOCIATED_TOKEN_PROGRAM_ID,
+   TOKEN_PROGRAM_ID,
+   Token,
+} = require("@solana/spl-token");
 
 const anchor = require('@project-serum/anchor');
 
@@ -13,12 +26,15 @@ export default function DoormanConfigInfo({ wallet, provider, program }) {
    const [configInfo, setConfigInfo] = useState({
       cost: null
    });
+   const [tokensLeft, setTokensLeft] = useState(-1);
+
 
    // init the wallets & connections and stuff
    useEffect(() => {
       (async () => {
          if (program) {
             let accountData = await program.account.config.fetch(DOORMAN_CONFIG);
+            const mintTokenVault = accountData.mintTokenVault;
             accountData.costInSol = accountData.costInLamports.toNumber() / anchor.web3.LAMPORTS_PER_SOL;
             accountData.whitelist = accountData.whitelist.toString();
             accountData.authority = accountData.authority.toString();
@@ -27,6 +43,18 @@ export default function DoormanConfigInfo({ wallet, provider, program }) {
             accountData.goLiveDate = dateTimeFormat.format(new Date(accountData.goLiveDate.toNumber() * 1000));
             console.log("\n >> config account data: ", accountData);
             setConfigInfo(accountData);
+
+            // see how many mint tokens are left in doorman's vault
+            const mintToken = new Token(
+               connection,
+               MINT,
+               TOKEN_PROGRAM_ID,
+               wallet.publicKey
+            );
+
+            let doormanMintTokenAccountInfo = await mintToken.getAccountInfo(mintTokenVault);
+            setTokensLeft(doormanMintTokenAccountInfo.amount.toNumber());
+
          }
       })();
    }, [program]);
@@ -48,6 +76,14 @@ export default function DoormanConfigInfo({ wallet, provider, program }) {
                   </Grid>
                   <Grid item xs={8}>
                      {configInfo.costInSol} SOL
+                  </Grid>
+                  <Grid item xs={4}>
+                     Mint Tokens Left:
+                  </Grid>
+                  <Grid item xs={8}>
+                     {tokensLeft > 0 && <span>{tokensLeft}</span>}
+                     {tokensLeft === 0 && <span>Sold Out!</span>}
+                     {tokensLeft < 0 && <span>Initializing...</span>}
                   </Grid>
                </Grid>
             </CardContent>
